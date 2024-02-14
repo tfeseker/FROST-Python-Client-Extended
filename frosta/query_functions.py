b988f59ddc223e6a769132893124edb2b2147900
tfeseker <feseker@posteo.net>
2024-02-14 20:58:22 +0100

    always return an entity list, even if empty or just a single entity

--- frosta/query_functions.py
+++ frosta/query_functions.py
@@ -62,13 +62,13 @@ def get_entity_list(entities, callback=None, step_size=None, **kwargs):
     query = add_order(query, **kwargs)
     query = add_chunks(query, **kwargs)
     entity_list = query.count().list(callback, step_size)
-    match len(entity_list.entities):
-        case 0:
-            return None
-        case 1:
-            return entity_list.get(0)
-        case _:
-            return entity_list
+    # match len(entity_list.entities):
+    #     case 0:
+    #         return None
+    #     case 1:
+    #         return entity_list.get(0)
+    #     case _:
+    #         return entity_list
 
 
 def add_filters(query, **kwargs):
